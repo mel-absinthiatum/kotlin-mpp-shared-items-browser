a4820d04d7a87756b5f99f6e3775cd2a2935189c
Natalie Savchenko <mel.absinthiatum@gmail.com>
2020-02-01 07:19:14 +0700

    Bump version to 1.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ import org.jetbrains.intellij.tasks.PatchPluginXmlTask
 import org.jetbrains.intellij.tasks.PublishTask
 
 group = "com.melabsinthiatum"
-version = "1.0.1"
+version = "1.0.2"
 
 buildscript {
     repositories { mavenCentral() }
